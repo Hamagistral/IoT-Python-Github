a38e80c8d5086a90c37fc588df37a48510401e0f
Hamza <hamza.lbelghiti@gmail.com>
2023-10-16 17:09:22 +0100

    Version 2: Remplaçement du Koala

--- zoo.py
+++ zoo.py
@@ -36,4 +36,12 @@ print(f"Dans le zoo, les deux animaux favoris par le public sont:\n 1. Le {koala
 
 lion.vieillir()
 
-print(f"Quand le lion {lion.get_name()} aura {lion.get_age()} ans, il sera déplaçer à un autre Zoo.")
+print(f"Quand le lion {lion.get_name()} aura {lion.get_age()} ans, il sera déplaçer à un autre Zoo.\n")
+
+koala.set_name("Max")
+koala.set_age(4)
+
+print(f"Le Koala Cooper vient d'être remplacé par {koala.get_name()} qui a {koala.get_age()} ans.\n")
+
+# Generation d'une erreur
+lion.set_age(2.5)
